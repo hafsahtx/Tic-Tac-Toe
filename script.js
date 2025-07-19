4d436836ac2b01139fbce7756a1d8272d4318ff8
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-07-19 09:00:40 +0100

    alternate between x and o, update screen and switch player

--- script.js
+++ script.js
@@ -16,10 +16,54 @@ function createGameboard(){
     return items;
 }
 
+function gameState(players){
+    //flow of game
+    let availableSpace = 9;
+    let activePlayer = players[0];
+    return {availableSpace, activePlayer};
+}
 
-let player1 = createPlayer("player1");
-let player2 = createPlayer("player2");
-console.log(player1)
-console.log(player2);
-let gameboard = createGameboard();
-console.group(gameboard)
+function changeActivePlayer(state,finishedTurn,players){
+    if(state.activePlayer.symbol === "X"&&finishedTurn===true){
+        state.activePlayer = players[1]
+    }else if(state.activePlayer.symbol === "O"&&finishedTurn===true){
+        state.activePlayer = players[0];
+    }
+}
+function updateBoard(square,state,players){
+    if(square){
+        if(state.activePlayer.symbol === "X"){
+            console.log("AAA")
+            square.innerHTML = "X";
+        }else{
+            console.log("BBBB")
+            square.innerHTML = "O";
+        }
+    }
+    changeActivePlayer(state,true,players)
+}
+
+function checkBoard(gameboard,state,players){
+    for(let item of gameboard){
+        item.addEventListener("click",(e)=>{
+            console.log(`square ${e.target.textContent} was selected`);
+            updateBoard(item, state,players);
+            return true;
+        })
+        
+    }
+    return false;
+}
+
+
+
+function playGame(){
+    let players = [createPlayer("player1"), createPlayer("player2")]
+    let gameboard = createGameboard();
+    let state = gameState(players);
+    let finishedTurn = checkBoard(gameboard,state,players);
+}
+
+
+
+playGame();
